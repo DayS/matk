1f6e998d6e50fd862bf45909c28d144719a1616a
Damien Villeneuve <contact@dvilleneuve.fr>
2019-12-11 14:11:03 +0100

    Use DI for Okhttp instances

--- src/main/kotlin/fr/matk/Main.kt
+++ src/main/kotlin/fr/matk/Main.kt
@@ -2,14 +2,18 @@ package fr.matk
 
 import com.github.ajalt.clikt.core.NoRunCliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.squareup.moshi.Moshi
 import fr.matk.command.apk.ApkCommand
 import fr.matk.service.Cache
+import okhttp3.OkHttpClient
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import java.io.File
 
 val matkModule = module {
     single { Cache(File(System.getProperty("user.home"), ".matk/cache")) }
+    single { OkHttpClient() }
+    single { Moshi.Builder().build() }
 }
 
 class Matk : NoRunCliktCommand()
--- src/main/kotlin/fr/matk/utils/Github.kt
+++ src/main/kotlin/fr/matk/utils/Github.kt
@@ -6,14 +6,16 @@ import com.squareup.moshi.Moshi
 import io.reactivex.Single
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 import java.io.IOException
 import java.net.URL
 
-object Github {
+object Github: KoinComponent {
 
     private val logger by LoggerDelegate()
-    private val client by lazy { OkHttpClient() }
-    private val moshi by lazy { Moshi.Builder().build() }
+    private val client by inject<OkHttpClient>()
+    private val moshi by inject<Moshi>()
     private val gistJsonAdapter by lazy { moshi.adapter(GithubResponse::class.java) }
 
     fun getReleaseUrl(repoOwner: String, repoName: String, version: String, filename: String) =
