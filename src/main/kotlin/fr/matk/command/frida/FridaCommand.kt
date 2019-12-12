0abe9500b0f3afa5b90974a1bd6ee08b76bce97c
Damien Villeneuve <contact@dvilleneuve.fr>
2019-12-12 22:54:20 +0100

    Add command to install Frida server on the device

---
+++ src/main/kotlin/fr/matk/command/frida/FridaCommand.kt
@@ -0,0 +1,12 @@
+package fr.matk.command.frida
+
+import com.github.ajalt.clikt.core.NoRunCliktCommand
+import com.github.ajalt.clikt.core.subcommands
+
+class FridaCommand : NoRunCliktCommand(name = "frida") {
+    init {
+        subcommands(
+            FridaInstallServerCommand()
+        )
+    }
+}
